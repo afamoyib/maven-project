1fb4fdfa275ceca9f161a9452ad763a455303be4
Afam Oyibo <afam.oyibo@gmail.com>
2018-07-29 21:46:20 -0400

    test

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,7 +12,10 @@ public class Greeter {
 
   }
 
-
+  /**
+   * @param This is a constructor.
+   * @return greeting
+   */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
